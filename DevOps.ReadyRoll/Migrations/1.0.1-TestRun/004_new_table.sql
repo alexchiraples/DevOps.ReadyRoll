e0f405224d2bcd14e766c42796a35b252261eeba
alexchiraples <alexandru.chiraples@ithub.gov.ro>
2017-07-11 20:51:37 +0300

    new test table

---
+++ DevOps.ReadyRoll/Migrations/1.0.1-TestRun/004_new_table.sql
@@ -0,0 +1,13 @@
+﻿-- <Migration ID="563025d4-c167-49f2-b7fd-67967979704e" />
+GO
+
+PRINT N'Creating [dbo].[AddTestTable]...';
+
+
+GO
+CREATE TABLE [dbo].[AddTestTable_new] (
+    [Id]   INT            NOT NULL,
+    [Col1] NVARCHAR (100) NOT NULL,
+    CONSTRAINT [PK_AddTestTable] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
