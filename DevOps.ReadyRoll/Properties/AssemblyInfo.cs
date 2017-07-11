301a97b0352572650a465dd6efc265ada62bed01
alexchiraples <alexandru.chiraples@ithub.gov.ro>
2017-07-11 21:43:18 +0300

    added assemblyinfo

---
+++ DevOps.ReadyRoll/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with the SQLCLR assembly.
+[assembly: AssemblyTitle("ReadyRoll POC")]
+[assembly: AssemblyDescription("ReadyRoll POC")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NA")]
+[assembly: AssemblyProduct("NA")]
+[assembly: AssemblyCopyright("NA")]
+[assembly: AssemblyTrademark("NA")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
